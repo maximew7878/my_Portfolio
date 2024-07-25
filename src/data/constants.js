d99e28db889d16ed23d2368fd7bc725935a060a7
Nikhil <nikhilsingh888787@gamil.com>
2024-07-25 10:05:57 +0530

    Initial project commit

---
+++ src/data/constants.js
@@ -0,0 +1,146 @@
+export const Bio = {
+    name: "Nikhil Singh",
+    roles: ["Frontend Developer", "Programmer"],
+    description:
+        "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+    github: "https://github.com/maximew7878",
+    resume: "https://drive.google.com/file/d/1D1GJTs-bMEujpJ4VPsiyOsMqsPabhfgl/view?usp=sharing",
+    linkedin: "https://www.linkedin.com/in/nikhilsingh8931/",
+    insta: "https://www.instagram.com/nikhill_singh1/",
+};
+
+export const skills = [
+    {
+        title: "Frontend",
+        skills: [
+            {
+                name: "React Js",
+                image: "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+            },
+            {
+                name: "HTML",
+                image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+            },
+            {
+                name: "CSS",
+                image: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+            },
+            {
+                name: "JavaScript",
+                image: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+            },
+        ],
+    },
+    {
+        title: "Others",
+        skills: [
+            {
+                name: "Git",
+                image: "https://git-scm.com/images/logos/downloads/Git-Icon-1788C.png",
+            },
+            {
+                name: "GitHub",
+                image: "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+            },
+            {
+                name: "Netlify",
+                image: "https://seeklogo.com/images/N/netlify-logo-BD8F8A77E2-seeklogo.com.png",
+            },
+            {
+                name: "VS Code",
+                image: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+            },
+            {
+                name: "C++",
+                image: "https://upload.wikimedia.org/wikipedia/commons/3/32/C%2B%2B_logo.png",
+            },
+        ],
+    },
+];
+
+export const experiences = [];
+
+export const education = [
+    {
+        id: 0,
+        img: "https://upload.wikimedia.org/wikipedia/en/b/b8/National_Institute_of_Technology%2C_Hamirpur_Logo.png",
+        school: "National Institute Of Technology, Hamirpur",
+        date: "Oct 2021 - Sep 2025",
+        grade: "7.6 CGPA",
+        desc: "I am currently pursuing a Bachelor's degree in Computer Science and Engineering at NIT Hamirpur. I have completed 6 semesters and have a CGPA of 7.6. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others. I am also a member of the Google Developers Student Club (GDSC) at KIIT, where I am learning and working on exciting projects with a team of talented developers.",
+        degree: "Bachelor of Technology - BTech, Computer Science and Engineering",
+    },
+    {
+        id: 1,
+        img: "https://www.bkdaldrichpublicschool.org.in/images/logo_03.png",
+        school: "B.K.D. Aldrich Public School,Orai",
+        date: "Apr 2019 - Apr 2021",
+        grade: "93.2%",
+        desc: "I completed my class 12 high school education at Aldrich Public School, Orai, where I studied Science.",
+        degree: "CBSE(XII), Science ",
+    },
+    {
+        id: 2,
+        img: "https://www.bkdaldrichpublicschool.org.in/images/logo_03.png",
+        school: "B.K.D. Aldrich Public School,Orai",
+        date: "Apr 2017 - Apr 2019",
+        grade: "91%",
+        desc: "I completed my class 10 education at Aldrich Public School,Orai.",
+        degree: "CBSE(X)",
+    },
+];
+
+export const projects = [
+    {
+        id: 3,
+        title: "Chat-Application",
+        date: "may 2024 - June 2024",
+        description: "This website is used to make friends and chat with them ",
+        image: "https://www.weblite.com.my/hubfs/blog-images/2019/december/8%20Reasons%20Why%20You%20Need%20Website%20Live%20Chat%20Now/livechat-1.jpg",
+        tags: [
+            "Docker",
+            "AWS",
+            "Eslint",
+            "tailwindcss",
+            "React Js",
+            "MongoDb",
+            "Node Js",
+            "Express Js",
+            "Redux",
+        ],
+        category: "web app",
+        github: "https://github.com/maximew7878/chat-application",
+    },
+    {
+        id: 0,
+        title: "Workout website",
+        date: "oct 2023 - oct 2023",
+        description:
+            "This is the workout website ,here u can find the exercise for all the parts which you want to trained.",
+        image: "https://i.pinimg.com/736x/c0/3f/f2/c03ff22e73610c6255c61fe07b4cf35e.jpg",
+        tags: ["HTML", "CSS", "Javascript", "React JS"],
+        category: "web app",
+        github: "https://github.com/maximew7878/Workout",
+    },
+    {
+        id: 1,
+        title: "Pet Care",
+        date: "july 2023 - Aug 2023",
+        description:
+            "This page is made for the pet care ,here you can upload your details and tell about the pet so that another people can take care of them.",
+        image: "https://w3layouts.com/wp-content/uploads/2021/06/dogs-care-website-template-1-768x480.jpg",
+        tags: ["React Js", "Javascript", "HTML", "CSS"],
+        category: "web app",
+        github: "https://github.com/maximew7878/Pet-care.github.io",
+    },
+    {
+        id: 2,
+        title: "GYM Enroolment Form",
+        date: "Aug 2022 - Aug 2022",
+        description: "This is the gym enroolment form",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR2JHu4pMCgRhA-ABmltTVtETHMTFoTBmN5cg&s",
+        tags: ["HTML", "CSS"],
+        category: "web app",
+        github: "https://github.com/maximew7878/GYm-Enrollment-Form-",
+    },
+];
